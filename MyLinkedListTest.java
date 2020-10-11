4a0f922c11aaf1fbfca5ff1bebc10342d1b98a9f
Mohana Kavya K <kkunisettymohana@student.nitw.ac.in>
2020-10-12 01:47:34 +0530

    Added : Functionality Added to Insert Nodes In Between Linked List

--- MyLinkedListTest.java
+++ MyLinkedListTest.java
@@ -39,5 +39,21 @@ public class MyLinkedListTest {
 				&& myLinkedList.tail.equals(thirdNode);
 		Assert.assertTrue(check);
 	}
+	
+	@Test
+	public void given3NumsInsertingSecondNumInLinkedListTest() {
+		MyNode<Integer> firstNode = new MyNode<Integer>(56);
+		MyNode<Integer> secondNode = new MyNode<Integer>(30);
+		MyNode<Integer> thirdNode = new MyNode<Integer>(70);
+		MyLinkedList myLinkedList = new MyLinkedList();
+		myLinkedList.add(firstNode);
+		myLinkedList.append(thirdNode);
+		myLinkedList.insert(firstNode,secondNode);
+		System.out.println("Inserting Node in Linked List");
+		myLinkedList.printMyNodes();
+		boolean result = myLinkedList.head.equals(firstNode) && myLinkedList.head.getNext().equals(secondNode)
+				&& myLinkedList.tail.equals(thirdNode);
+		Assert.assertTrue(result);
+	}
 
 }
